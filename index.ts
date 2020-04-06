041c52703cc2a87285df5dabef36b98d48731eea
Steve Mao <maochenyan@gmail.com>
2020-04-06 11:54:05 +1000

    tweaks

--- index.ts
+++ index.ts
@@ -1,19 +1,19 @@
 import { pipe } from 'fp-ts/lib/pipeable';
-import * as TE from 'fp-ts/lib/TaskEither';
-import * as t from 'io-ts';
+import { TaskEither, fromEither, mapLeft } from 'fp-ts/lib/TaskEither';
+import { Decoder } from 'io-ts';
 import toError from 'to-error';
 
 const decode = <N>(
-  T: t.Decoder<unknown, N>,
+  T: Decoder<unknown, N>,
   data: unknown,
-): TE.TaskEither<Error, N> =>
+): TaskEither<Error, N> =>
   pipe(
     pipe(
       data,
       T.decode,
-      TE.fromEither
+      fromEither
     ),
-    TE.mapLeft(toError)
+    mapLeft(toError)
   );
 
 export default decode;
